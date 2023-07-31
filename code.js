662f9fc58e229d3e72b8438d471632fff6b5585e
Diego250x <113477900+Diego250x@users.noreply.github.com>
2023-07-30 20:39:39 -0500

    code.js

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
-a
+<script>
+const js_thml = () => {
+	let code = "";
+	code += "<a href=\"http://srt.am/bdsjsH\">Example</a>\n";
+	return code;
+}
+
+
+document.getElementById("js_html").innerText = js_thml();
+document.getElementById("html-text").value = js_thml();
+</script>
